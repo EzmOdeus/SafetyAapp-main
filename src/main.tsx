f4ec03cfd496f8e2ba3ac9fd01b9db265d1aafc2
EzmOdeus <87050190+EzmOdeus@users.noreply.github.com>
2026-01-14 00:12:00 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,13 @@ import { KindeProvider } from '@kinde-oss/kinde-auth-react';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <KindeProvider
-		clientId="e25d85e213ee4fb4bba5d60a08cb0e62"
-		domain="https://safetyapp.kinde.com"
-		redirectUri="https://safety-aapp-main.vercel.app/"
-		logoutUri="https://safety-aapp-main.vercel.app/"
-	>
-
-    <App />
-  </KindeProvider>
+      clientId="e25d85e213ee4fb4bba5d60a08cb0e62"
+      domain="https://safetyapp.kinde.com"
+      redirectUri={window.location.origin}
+      logoutUri={window.location.origin}
+      isDangerouslyUseLocalStorage={true}
+    >
+      <App />
+    </KindeProvider>
   </StrictMode>
 );
